7d6f70a3d64c33329b5d3b7da572354742dbc50a
Léo <leo.schmuck@gmail.com>
2023-01-12 18:49:07 +0100

    Correction fichier init BDD

--- Serveur/gestionBdd/initDataBdd.js
+++ Serveur/gestionBdd/initDataBdd.js
@@ -14,13 +14,3 @@ const joueurSchema = {
 };
 
 const Joueur = mongoose.model('joueurs', joueurSchema);
-
-const joueur1 = new Joueur({pseudo : "jonny", password : "jonny", score : 0});
-const joueur2 = new Joueur({pseudo : "cash", password : "cash", score : 0});
-const joueur3 = new Joueur({pseudo : "test", password : "test", score : 0});
-const joueur4 = new Joueur({pseudo : "hihelo", password : "hihelo", score : 0});
-
-joueur1.save();
-joueur2.save();
-joueur3.save();
-joueur4.save();
